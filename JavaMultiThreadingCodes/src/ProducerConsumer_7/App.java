b531a8a292410a4cbc264d700e80acf5aadd01a7
IOAyman <theycallmethedr@gmail.com>
2015-01-09 10:42:53 +0100

    CountDownLatch_7.App#App Clean up auto-generated catch blocks re-activated the 0.1sec pause in consumer() (for the sake of not flooding the output terminal)
    Commented out {t1,t2}.join() ... instead, forced the app (main thread) to quite after 30secs

--- JavaMultiThreadingCodes/src/ProducerConsumer_7/App.java
+++ JavaMultiThreadingCodes/src/ProducerConsumer_7/App.java
@@ -32,16 +32,12 @@ public class App {
     private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
 
     public static void main(String[] args) throws InterruptedException {
-        /**
-         *
-         */
+
         Thread t1 = new Thread(new Runnable() {
             public void run() {
                 try {
                     producer();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                } catch (InterruptedException ignored) {}
             }
         });
 
@@ -49,16 +45,17 @@ public class App {
             public void run() {
                 try {
                     consumer();
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+                } catch (InterruptedException ignored) {}
             }
         });
         t1.start();
         t2.start();
-        t1.join();
-        t2.join();
+//        t1.join();
+//        t2.join();
+
+        // Pause for 30 seconds and force quitting the app (because we're looping infinitely)
+        Thread.sleep(30000);
+        System.exit(0);
     }
 
     private static void producer() throws InterruptedException {
@@ -71,7 +68,7 @@ public class App {
     private static void consumer() throws InterruptedException {
         Random random = new Random();
         while (true) {
-            //Thread.sleep(100);
+            Thread.sleep(100);
             if (random.nextInt(10) == 0) {
                 Integer value = queue.take();//if queue is empty waits
                 System.out.println("Taken value: " + value + "; Queue size is: " + queue.size());
